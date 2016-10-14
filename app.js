9a4c2e02ab2a98412de80638cbe7feaf54473135
Ina <inarancic@yahoo.com>
2016-10-14 15:05:08 +0200

    '**/*'

---
+++ app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController ($scope) {
+
+  var comma = ',';
+  function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+    console.log(arrayOfStrings.length) ;
+    return arrayOfStrings.length;
+  }
+  $scope.howMuch = function(){
+    console.log($scope.dishes);
+    if($scope.dishes){
+      var dishesNum = splitString($scope.dishes, comma);
+      if(dishesNum<=3){
+        $scope.message = "Enjoy!";
+      }else{
+        $scope.message = "Too much!";
+      }
+      $scope.color = "green";
+    }else{
+      $scope.message = "Please enter data first";
+      $scope.color = "red";
+    }
+
+  }
+
+}
+
+})();
